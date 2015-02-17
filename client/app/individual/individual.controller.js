8c0b4ed6b362de69e2f9025b1949bb5033114093
Preston Miller <mill6528@morris.umn.edu>
2015-02-17 12:19:27 -0600

    Got data on individual page

--- client/app/individual/individual.controller.js
+++ client/app/individual/individual.controller.js
@@ -1,6 +1,46 @@
 'use strict';
 
 angular.module('jsonDataProcessingLabWithResaThomasJessPrestonApp')
-  .controller('IndividualCtrl', function ($scope) {
-    $scope.message = 'Hello';
+  .controller('IndividualCtrl', function ($scope, $http, socket) {
+    $scope.data = [];
+
+///
+    $scope.getStudents = function() {
+      $http.get('/api/student').success(function (student) {
+        $scope.data = student;
+      });
+    };
+    $scope.getStudents();
+///
+
+    $scope.calculateGPA = function(student){
+      if (array.length == 0) {
+        return 0;
+      }
+      var pointsEarned = 0;
+      var totalCredits = 0;
+      for (var index = 0; index < student.courses.length; index++) {
+        var course = student[index];
+        pointsEarned += course.credits * $scope.letterToNum(course.grade);
+        totalCredits += course.credits;
+      }
+      return pointsEarned/totalCredits;
+    }
+
+    $scope.letterToNum = function(letter){
+      letter = letter.toUpperCase();
+      switch(letter) {
+        case "A":
+          return 4.0;
+        case "B":
+          return 3.0;
+        case "C":
+          return 2.0;
+        case "D":
+          return 1.0;
+        default:
+          return 0.0;
+      }
+    }
+
   });
--- client/app/individual/individual.controller.spec.js
+++ client/app/individual/individual.controller.spec.js
@@ -4,18 +4,42 @@ describe('Controller: IndividualCtrl', function () {
 
   // load the controller's module
   beforeEach(module('jsonDataProcessingLabWithResaThomasJessPrestonApp'));
+  beforeEach(module('socketMock'));
 
-  var IndividualCtrl, scope;
+  var IndividualCtrl,
+    scope,
+    $httpBackend;
 
   // Initialize the controller and a mock scope
-  beforeEach(inject(function ($controller, $rootScope) {
+  beforeEach(inject(function (_$httpBackend_, $controller, $rootScope) {
+    $httpBackend = _$httpBackend_;
+    $httpBackend.expectGET('/api/things')
+      .respond(['HTML5 Boilerplate', 'AngularJS', 'Karma', 'Express']);
+
     scope = $rootScope.$new();
     IndividualCtrl = $controller('IndividualCtrl', {
       $scope: scope
     });
   }));
 
-  it('should ...', function () {
-    expect(1).toEqual(1);
+
+  // it('should calculate a students GPA based on their class grades and class credits', function () {
+  //   expect(scope.calculateGPA()).toBe(4);
+  // });
+
+  it('should convert a grade letter to a credit value', function () {
+    expect(scope.letterToNum("A")).toBe(4);
+    expect(scope.letterToNum("B")).toBe(3);
+    expect(scope.letterToNum("C")).toBe(2);
+    expect(scope.letterToNum("D")).toBe(1);
+    expect(scope.letterToNum("F")).toBe(0);
+
+    expect(scope.letterToNum("a")).toBe(4);
+    expect(scope.letterToNum("b")).toBe(3);
+    expect(scope.letterToNum("c")).toBe(2);
+    expect(scope.letterToNum("d")).toBe(1);
+    expect(scope.letterToNum("f")).toBe(0);
   });
+
+
 });
