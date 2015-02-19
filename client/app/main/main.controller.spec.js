18508c51242c454d196efbdb4d0d4a3481a66dcd
rabid-angel <hagen715@morris.umn.edu>
2015-02-19 12:32:59 -0600

    Finished writing tests for CalculateNumberOfCredits, and getGradeLevel, as well as finished the functions.

--- client/app/main/main.controller.spec.js
+++ client/app/main/main.controller.spec.js
@@ -7,8 +7,8 @@ describe('Controller: MainCtrl', function () {
   beforeEach(module('socketMock'));
 
   var MainCtrl,
-      scope,
-      $httpBackend;
+    scope,
+    $httpBackend;
 
   // Initialize the controller and a mock scope
   beforeEach(inject(function (_$httpBackend_, $controller, $rootScope) {
@@ -38,9 +38,8 @@ describe('Controller: MainCtrl', function () {
   });
 
 
-
 //Tests for CalculateGPA
-  it('these results should yield 4.0', function(){
+  it('these results should yield 4.0', function () {
     var theOneClassAStudent = {
       "firstName": "Love",
       "lastName": "Roberts",
@@ -58,11 +57,12 @@ describe('Controller: MainCtrl', function () {
             "credits": 4
           },
           "grade": "A"
-        }]};
+        }]
+    };
     expect(scope.calculateGPA(theOneClassAStudent)).toBeCloseTo(4.0, 2); // This makes sure we have 2 decimal points of precision.
   })
 
-  it('these results should yield 2.0', function(){
+  it('these results should yield 2.0', function () {
     var theTwoClassStudent = {
       "firstName": "Love",
       "lastName": "Roberts",
@@ -89,8 +89,92 @@ describe('Controller: MainCtrl', function () {
             "credits": 4
           },
           "grade": "D"
-        }]};
+        }]
+    };
     expect(scope.calculateGPA(theTwoClassStudent)).toBeCloseTo(2.5, 2);
   })
 
-});
+
+//Tests for CalculateNumberOrCredits
+  it('these results should yield 4.0', function () {
+    var theOneClassAStudent = {
+      "firstName": "Love",
+      "lastName": "Roberts",
+      "dateOfBirth": "1989-01-18",
+      "gender": "male",
+      "email": "loveroberts@eweville.com",
+      "phone": "+1 (875) 519-3316",
+      "address": "562 Eastern Parkway, Crayne, Virginia, 5152",
+      "courses": [
+        {
+          "course": {
+            "name": "Models of Computing Systems",
+            "subject": "CSCI",
+            "courseNumber": 3401,
+            "credits": 4
+          },
+          "grade": "A"
+        }]
+    };
+    expect(scope.calculateNumberOfCredits(theOneClassAStudent)).toBeCloseTo(4.0, 2); // This makes sure we have 2 decimal points of precision.
+  })
+
+  it('these results should yield 8', function () {
+    var theTwoClassStudent = {
+      "firstName": "Love",
+      "lastName": "Roberts",
+      "dateOfBirth": "1989-01-18",
+      "gender": "male",
+      "email": "loveroberts@eweville.com",
+      "phone": "+1 (875) 519-3316",
+      "address": "562 Eastern Parkway, Crayne, Virginia, 5152",
+      "courses": [
+        {
+          "course": {
+            "name": "Models of Computing Systems",
+            "subject": "CSCI",
+            "courseNumber": 3401,
+            "credits": 4
+          },
+          "grade": "A"
+        },
+        {
+          "course": {
+            "name": "Software Design and Development",
+            "subject": "CSCI",
+            "courseNumber": 3601,
+            "credits": 4
+          },
+          "grade": "D"
+        }]
+    };
+    expect(scope.calculateNumberOfCredits(theTwoClassStudent)).toBeCloseTo(8, 2);
+  })
+
+
+  it('these results should yield freshman', function () {
+
+    expect(scope.getGradeYear(29)).toBe("Freshman");
+    expect(scope.getGradeYear(1)).toBe("Freshman");
+  })
+
+  it('these results should yield Sophomore', function () {
+
+    expect(scope.getGradeYear(30)).toBe("Sophomore");
+    expect(scope.getGradeYear(59)).toBe("Sophomore");
+  })
+
+  it('these results should yield Junior', function () {
+
+    expect(scope.getGradeYear(89)).toBe("Junior");
+    expect(scope.getGradeYear(60)).toBe("Junior");
+  })
+
+  it('these results should yield Senior', function () {
+
+    expect(scope.getGradeYear(90)).toBe("Senior");
+    expect(scope.getGradeYear(120)).toBe("Senior");
+  })
+
+})
+
